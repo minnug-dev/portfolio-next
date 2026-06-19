35b1ba64cb8ddb34af4e78e5e06caf8dca2f1997
minnug <parkminnug@gmail.com>
2026-06-19 15:10:20 +0900

    fix: About 컨텐츠 수정

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,43 +5,19 @@ import { aboutText } from '@/constants';
 
 const about = () => {
   return (
-    <section id="about">
+    <section id="about" className="about">
       <div className="about__inner">
-        <h2 className="about__title">{aboutText.title}</h2>
-        <div className="about__lines" aria-hidden="true">
-          <span className="line"></span>
-          <span className="line"></span>
-          <span className="line"></span>
-          <span className="line"></span>
-          <span className="line"></span>
-          <span className="line"></span>
-          <span className="line"></span>
+        <div className="img-wrap">
+          <Image src={aboutText.img} alt="민주 그림체 프로필 사진" style={{ width: '100%', height: '100%' }} />
         </div>
-        <div className="about__text">
-          <div className="text">
-            <div>{aboutText.desc[0]}</div>
-            <div>{aboutText.desc[1]}</div>
-            <div>{aboutText.desc[2]}</div>
-            <div>effort</div>
+        <div className="text-wrap">
+          <h3 className="sub-tit">{aboutText.stit}</h3>
+          <div className="desc">
+            <p>{aboutText.desc[0]}</p>
+            <p>{aboutText.desc[1]}</p>
+            <p>{aboutText.desc[2]}</p>
+            <p>{aboutText.desc[3]}</p>
           </div>
-          <div className="img">
-            <Image
-              src={aboutText.img}
-              alt="어바웃미"
-              width={300}
-              height={300}
-              style={{ width: '100%', height: '100%' }}
-            />
-          </div>
-        </div>
-        <div className="about__lines bottom" aria-hidden="true">
-          <span className="line"></span>
-          <span className="line"></span>
-          <span className="line"></span>
-          <span className="line"></span>
-          <span className="line"></span>
-          <span className="line"></span>
-          <span className="line"></span>
         </div>
       </div>
     </section>
