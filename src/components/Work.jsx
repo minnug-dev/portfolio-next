713cd39b7052814af97cf312375e95bac6f69e12
minnug <parkminnug@gmail.com>
2026-07-01 11:11:36 +0900

    fix: Work 컴포넌트 수정

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 
 import { workText } from '@/constants';
@@ -6,13 +7,22 @@ const work = () => {
   return (
     <section id="work">
       <div className="work__inner">
-        <h2 className="work__title">Work</h2>
-        <div className="work__desc">
+        <h2 className="sub-tit">Work</h2>
+        <div className="work-list">
           {workText.map((work, key) => (
-            <div key={key}>
-              <span>{key + 1}.</span>
-              <h3>{work.title}</h3>
-              <p>{work.desc}</p>
+            <div key={key} className="list-item">
+              <a href={work.link} target="_blank">
+                <div className="img-wrap">
+                  <Image src={work.img} alt={work.imgDesc} style={{ width: '100%', height: 'auto' }} />
+                </div>
+                <h3>{work.title}</h3>
+                <p>{work.desc}</p>
+                <div className="tool-wrap">
+                  {work.tool.map((tool, index) => (
+                    <span key={index}>{tool}</span>
+                  ))}
+                </div>
+              </a>
             </div>
           ))}
         </div>
