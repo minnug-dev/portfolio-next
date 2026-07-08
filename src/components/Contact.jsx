b61fbaa1527b4a239248e74b00d23ea7d4c70568
minnug <parkminnug@gmail.com>
2026-07-08 14:11:04 +0900

    fix: Contact 컴포넌트 gsap 관련 버그 수정

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,15 +7,17 @@ gsap.registerPlugin(ScrollTrigger);
 
 const Contact = () => {
   const contactRef = useRef(null);
+  const triggerRef = useRef(null);
 
   useEffect(() => {
     const ctx = gsap.context(() => {
       ScrollTrigger.create({
-        trigger: contactRef.current,
-        start: 'top top',
-        end: 'bottom center',
+        trigger: triggerRef.current,
+        start: 'bottom bottom',
+        end: 'bottom top',
+        markers: true,
         toggleClass: {
-          targets: 'header',
+          targets: '.header__logo',
           className: 'contact-active',
         },
       });
@@ -25,28 +27,39 @@ const Contact = () => {
   }, []);
 
   return (
-    <section id="contact" className="contact" ref={contactRef}>
-      <div className="contact__inner">
-        <div className="contact__cont">
-          <div>
-            <h2 className="sub-tit">Contact</h2>
-            <span className="name">{contactText.name}</span>
-          </div>
-          <div className="info">
-            {contactText.contact.map((link, index) => (
-              <div key={index} className="menu">
-                <h3>{link.title}</h3>
-                {link.links.map((item, idx) => (
-                  <a key={idx} href={item.link} target="_blank" rel="noopener noreferrer">
-                    {item.name}
-                  </a>
-                ))}
-              </div>
-            ))}
+    <>
+      <div ref={triggerRef} id="contact" className="contact-trigger visually-hidden" aria-hidden="true" />
+
+      <section className="contact" ref={contactRef}>
+        <div className="contact__inner">
+          <div className="contact__cont">
+            <div>
+              <h2 className="sub-tit">Contact</h2>
+              <span className="name">{contactText.name}</span>
+            </div>
+            <div className="info">
+              {contactText.contact.map((link, index) => (
+                <div key={index} className="menu">
+                  <h3>{link.title}</h3>
+                  {link.links.map((item, idx) => (
+                    <a
+                      key={idx}
+                      href={item.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      data-cursor
+                      data-cursor-size="50"
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
