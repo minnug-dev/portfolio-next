1ecf34e1af60b4cd4e4a57f4f9721002e407a6f7
minnug <parkminnug@gmail.com>
2026-07-08 14:13:30 +0900

    fix: Work 컴포넌트 코드 수정

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,26 +3,26 @@ import React from 'react';
 
 import { workText } from '@/constants';
 
-const work = () => {
+const Work = () => {
   return (
-    <section id="work">
+    <section id="work" className="work">
       <div className="work__inner">
         <h2 className="sub-tit">Work</h2>
         <div className="work-list">
           {workText.map((work, key) => (
             <div key={key} className="list-item">
-              <a href={work.link} target="_blank">
+              <a href={work.link} target="_blank" data-cursor data-cursor-size="100" data-cursor-text="View">
                 <div className="img-wrap">
                   <Image src={work.img} alt={work.imgDesc} style={{ width: '100%', height: 'auto' }} />
                 </div>
-                <h3>{work.title}</h3>
-                <p>{work.desc}</p>
-                <div className="tool-wrap">
-                  {work.tool.map((tool, index) => (
-                    <span key={index}>{tool}</span>
-                  ))}
-                </div>
               </a>
+              <h3>{work.title}</h3>
+              <p>{work.desc}</p>
+              <div className="tool-wrap">
+                {work.tool.map((tool, index) => (
+                  <span key={index}>{tool}</span>
+                ))}
+              </div>
             </div>
           ))}
         </div>
@@ -31,4 +31,4 @@ const work = () => {
   );
 };
 
-export default work;
+export default Work;
