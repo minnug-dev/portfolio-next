d1c21a0c9e8bc67e405e19f5cc24108cc16c9960
minnug <parkminnug@gmail.com>
2026-07-09 20:01:11 +0900

    fix: About 컴포넌트 수정

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,6 +2,8 @@ import Image from 'next/image';
 import React from 'react';
 
 import { aboutText } from '@/constants';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLink } from '@fortawesome/free-solid-svg-icons';
 
 const About = () => {
   return (
@@ -16,27 +18,43 @@ const About = () => {
               priority
             />
           </div>
-          <div className="txt-wrap">
+          <div className="text-wrap">
             <h2 className="sub-tit">{aboutText.stit}</h2>
-            <div className="desc">
-              <p>{aboutText.desc[0]}</p>
-              <p>{aboutText.desc[1]}</p>
-              <p>{aboutText.desc[2]}</p>
-              <p>{aboutText.desc[3]}</p>
+            <div className="info">
+              <p>{aboutText.info[0]}</p>
+              <p>{aboutText.info[1]}</p>
+              <p>{aboutText.info[2]}</p>
+              <p>{aboutText.info[3]}</p>
             </div>
           </div>
         </div>
         <ul className="history-list">
           {aboutText.history.map((history, index) => (
             <li key={index} className="history-item">
-              <h3>{history.title}</h3>
+              <h3>
+                <FontAwesomeIcon icon={history.icon} className="icon" />
+                {history.tit}
+              </h3>
               <div className="item">
-                {history.list.map((item, idx) => (
-                  <div key={idx}>
+                {history.list.map((item, index) => (
+                  <div key={index}>
                     <span className="date">{item.date}</span>
-                    <div className="txt-wrap">
+                    <div className="text-wrap">
                       <h4 className="name">{item.name}</h4>
                       <p className="desc">{item.desc}</p>
+                      {item.link && (
+                        <a
+                          href={item.link.url}
+                          className="link"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          data-cursor
+                          data-cursor-size="50"
+                        >
+                          <FontAwesomeIcon icon={faLink} className="icon" />
+                          {item.link.label}
+                        </a>
+                      )}
                     </div>
                   </div>
                 ))}
