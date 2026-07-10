2957e411cc1dc0321d30eb6855be6d523bada2f2
minnug <parkminnug@gmail.com>
2026-07-10 20:05:24 +0900

    feat: 글자 애니메이션 추가

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useLayoutEffect } from 'react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
@@ -7,10 +7,32 @@ import { introText } from '@/constants';
 gsap.registerPlugin(ScrollTrigger);
 
 const Intro = () => {
+  const containerRef = useRef(null);
   const scrollRef = useRef(null);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     const ctx = gsap.context(() => {
+      gsap.set(['.intro__title .text', '.intro__title .tit p'], {
+        clipPath: 'inset(0 100% 0 0)',
+      });
+
+      const tl = gsap.timeline({
+        defaults: { ease: 'power3.easeInOut' },
+      });
+
+      tl.to('.intro__title .text', {
+        clipPath: 'inset(0 0% 0 0)',
+        duration: 4,
+      }).to(
+        '.intro__title .tit p',
+        {
+          clipPath: 'inset(0 0% 0 0)',
+          duration: 1.3,
+          stagger: 0.3,
+        },
+        '-=3.5',
+      );
+
       ScrollTrigger.create({
         trigger: 'body',
         start: '500',
@@ -32,23 +54,27 @@ const Intro = () => {
           });
         },
       });
-    });
+    }, containerRef);
 
     return () => ctx.revert();
   }, []);
 
   return (
-    <section id="intro" className="intro">
+    <section id="intro" className="intro" ref={containerRef}>
       <div className="intro__inner">
         <h2 className="visually-hidden">박민주 포트폴리오 인트로</h2>
         <div className="intro__title">
-          <span className="text">{introText.text}</span>
+          <div className="text">
+            {introText.text}
+            <span className="icon">👋</span>
+          </div>
           <div className="tit">
             <p>{introText.tit[0]}</p>
             <p>{introText.tit[1]}</p>
             <p>{introText.tit[2]}</p>
           </div>
         </div>
+
         <div className="scroll" ref={scrollRef}>
           <span className="label">Scroll Down</span>
           <div className="bar">
