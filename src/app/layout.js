5d19e7cd9d0c5ea34482ca1313ff7c718e8e8326
minnug <parkminnug@gmail.com>
2026-07-13 15:57:35 +0900

    remove: 불필요한 소스 삭제

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,6 @@ const pretendard = localFont({
 export const metadata = {
   title: '박민주 포트폴리오',
   description: 'Next.js로 제작한 박민주 포트폴리오 사이트입니다.',
-  keywords: ['포트폴리오', '넥스트', 'portfolio', 'next.js'],
 };
 
 export default function RootLayout({ children }) {
