3a5792eeaf943d7260905b631414edf3397e0314
minnug <parkminnug@gmail.com>
2026-09-06 16:31:30 +0900

    feat: swiper 네비게이션 추가

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,25 +1,30 @@
+import { useState } from 'react';
 import Image from 'next/image';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination, EffectFade } from 'swiper/modules';
+import { Pagination, EffectFade, Navigation } from 'swiper/modules';
 import { projectText } from '@/constants';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCalendar, faScrewdriverWrench } from '@fortawesome/free-solid-svg-icons';
+import { faCalendar, faScrewdriverWrench, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 import 'swiper/css';
 import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import 'swiper/css/effect-fade';
 
 const Project = () => {
+  const [swiperInstance, setSwiperInstance] = useState(null);
+
   return (
     <section id="project" className="project">
       <div className="project__inner">
         <h2 className="sub-tit mono">{projectText.stit}</h2>
         <div className="project__list">
           <Swiper
-            modules={[Pagination, EffectFade]}
+            modules={[Pagination, EffectFade, Navigation]}
             slidesPerView={1}
             effect="fade"
             fadeEffect={{ crossFade: true }}
-            speed={1500}
+            speed={500}
             loop={true}
             grabCursor={true}
             mousewheel={{
@@ -31,13 +36,26 @@ const Project = () => {
               bulletClass: 'project-bullet',
               bulletActiveClass: 'project-bullet--active',
             }}
+            navigation={{
+              prevEl: '.project-nav-prev',
+              nextEl: '.project-nav-next',
+            }}
+            onSwiper={(swiper) => {
+              setSwiperInstance(swiper);
+            }}
+            onBeforeInit={(swiper) => {
+              if (typeof swiper.params.navigation !== 'boolean') {
+                swiper.params.navigation.prevEl = '.project-nav-prev';
+                swiper.params.navigation.nextEl = '.project-nav-next';
+              }
+            }}
             className="project-swiper"
           >
             {projectText.list.map((project, index) => (
               <SwiperSlide key={index}>
-                <article className={`list__item s${index + 1}`} key={index}>
+                <article className={`list__item s${index + 1}`}>
                   <div className="img-wrap">
-                    <Image src={project.img.src} alt={project.img.alt} style={{ width: '100%', height: '100%' }} />
+                    <Image src={project.img.src} alt={project.img.alt} fill style={{ objectFit: 'cover' }} />
                   </div>
                   <div className="text-wrap">
                     <div>
@@ -50,9 +68,9 @@ const Project = () => {
                         <p>{project.text.desc}</p>
                       </div>
                       <div className="btn">
-                        {project.buttons.map((button, index) => (
+                        {project.buttons.map((button, btnIndex) => (
                           <a
-                            key={index}
+                            key={btnIndex}
                             href={button.href}
                             target="_blank"
                             rel="noopener noreferrer"
@@ -82,6 +100,12 @@ const Project = () => {
 
             <div className="project-pagination"></div>
           </Swiper>
+          <button className="project-nav-prev" aria-label="이전 프로젝트">
+            <FontAwesomeIcon icon={faChevronLeft} />
+          </button>
+          <button className="project-nav-next" aria-label="다음 프로젝트">
+            <FontAwesomeIcon icon={faChevronRight} />
+          </button>
         </div>
       </div>
     </section>
